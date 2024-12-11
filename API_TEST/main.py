5eb36b2e7468aa53cb587f05f1b6f8a192da9796
hds <0117ds@gmail.com>
2024-12-11 11:43:54 +0900

    20241211

--- API_TEST/main.py
+++ API_TEST/main.py
@@ -1,12 +1,14 @@
 ############################################################################################
-# python 설치: pip3 install fastapi "uvicorn[standard]"
+# python 설치:
+# - pip3 install fastapi "uvicorn[standard]"
+# - pip install pytest
 # 서버 실행방법(터미널): uvicorn main:app <= uvicorn 서버는 python에서 제공해주는 API 서버
 # 웹 확인
 # - http://localhost:8000/API_TEST
 # - http://localhost:8000/API_TEST/fast_api/5?q=somequery2
 # Swagger 경로: http://localhost:8000/docs
 # ReDoc 경로: http://localhost:8000/redoc
-# 최초작성일자: 2024.02.10
+# 최초 작성일자: 2024.02.10
 ############################################################################################
 
 from fastapi import FastAPI
@@ -14,27 +16,27 @@ from typing import Union, Optional, List
 from pydantic import BaseModel
 from uuid import UUID, uuid1, uuid4
 from enum import Enum
-from models import User
+#from models import User
 
-class Gender(str, Enum):
-    male = 'male'
-    female = 'female'
-
-
-class Role(str, Enum):
-    admin = 'admin'
-    user = 'user'
-    student = 'student'
-
-
-class User(BaseModel):
-    id: Optional[UUID] = uuid1() # uuid1은 타임스탬프를 기준으로 생성, uuid4은 랜덤 생성 방식 생성
-    id2: Optional[UUID] = uuid4() # uuid1은 타임스탬프를 기준으로 생성, uuid4은 랜덤 생성 방식 생성
-    first_name: str
-    last_name: str
-    middle_name: Optional[str]
-    gender: Gender
-    roles: List[Role]
+# class Gender(str, Enum):
+#     male = 'male'
+#     female = 'female'
+#
+#
+# class Role(str, Enum):
+#     admin = 'admin'
+#     user = 'user'
+#     student = 'student'
+#
+#
+# class User(BaseModel):
+#     id: Optional[UUID] = uuid1() # uuid1은 타임스탬프를 기준으로 생성, uuid4은 랜덤 생성 방식 생성
+#     id2: Optional[UUID] = uuid4() # uuid1은 타임스탬프를 기준으로 생성, uuid4은 랜덤 생성 방식 생성
+#     first_name: str
+#     last_name: str
+#     middle_name: Optional[str]
+#     gender: Gender
+#     roles: List[Role]
 
 
 class Item(BaseModel):
@@ -45,9 +47,9 @@ class Item(BaseModel):
 
 app = FastAPI() # 인스턴스 생성
 
-db: List[User] = [
-    User(id=uuid4(), first_name='hond', last_name='do sung')
-]
+# db: List[User] = [
+#     User(id=uuid4(), first_name='hond', last_name='do sung')
+# ]
 
 
 kk = list()
